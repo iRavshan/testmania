2ea4f79a5b380f5b65ac1349a11d4cb668c2e95d
iRavshan <ravshan4ikov@gmail.com>
2023-05-08 21:25:44 +0500

    create staticfiles dir

--- src/testmania/settings.py
+++ src/testmania/settings.py
@@ -121,7 +121,9 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/uploads/'
 
