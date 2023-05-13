980d351035662644793f30e53654ba1dc379333e
iRavshan <ravshan4ikov@gmail.com>
2023-05-13 14:34:04 +0500

    adfsf

--- src/account/models.py
+++ src/account/models.py
@@ -5,3 +5,4 @@ import uuid
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     study_center = models.TextField(default=None, null=True)
+    avatar = models.ImageField(upload_to='avatars', null=True)
