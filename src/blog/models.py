4ccc9c226a8490c947771a163d4161a58391fb86
iRavshan <ravshan4ikov@gmail.com>
2023-05-22 17:06:29 +0500

    asdasda

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,6 +3,12 @@ from uuid import uuid4
 from account.models import CustomUser
 from ckeditor.fields import RichTextField
 
+class Tag(models.Model):
+    name = models.CharField(max_length=60, null=False)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid4())
     thumbnail = models.FileField(upload_to='blog/thumbnails', null=True)
@@ -11,6 +17,8 @@ class Post(models.Model):
     title = models.CharField(max_length=300, null=False)
     description = RichTextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
+
