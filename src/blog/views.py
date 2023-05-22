7e1a470857b240655ac50c5d206bfdada38122fe
iRavshan <ravshan4ikov@gmail.com>
2023-05-22 17:53:54 +0500

    SAdada

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.db.models import Q
 
 def Home(request):
     posts = Post.objects.all().order_by('-created_at').values()
@@ -9,8 +10,12 @@ def Home(request):
     return render(request, 'blog/blog.html', context)
 
 def SingleBlog(request, id):
-    post = Post.objects.get(id=id)
+    post = get_object_or_404(Post, id=id)
+    tags = post.tags.all()
+    related_to_topic = []
+    similar_posts = Post.objects.filter(tags__name__icontains=tags[0].name)
     context = {
-        'post': post
+        'post': post,
+        'related_to_topic': similar_posts
     }
     return render(request, 'blog/single-blog.html', context)
